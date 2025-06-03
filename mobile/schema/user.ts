2ee49c4fd3989b3bd9c43b9fc387560278a106d9
ZJ <zhijietan97@gmail.com>
2025-06-03 22:44:09 +0800

    feat(mobile): add update user flow

--- mobile/schema/user.ts
+++ mobile/schema/user.ts
@@ -4,11 +4,17 @@ export const signUpSchema = z.object({
   firstName: z.string().min(1, "Cannot be empty"),
   lastName: z.string().min(1, "Cannot be empty"),
   username: z.string().min(1, "Cannot be empty"),
-  email: z.string().min(1, "Cannot be empty"),
+  email: z.string().email(),
   password: z.string().min(1, "Cannot be empty"),
 });
 
 export const loginSchema = z.object({
   email: z.string().min(1, "Cannot be empty"),
   password: z.string().min(1, "Cannot be empty"),
+});
+
+export const userUpdateSchema = z.object({
+  firstName: z.string().min(1, "Cannot be empty"),
+  lastName: z.string().min(1, "Cannot be empty"),
+  username: z.string().min(1, "Cannot be empty"),
 });
